b9ba5c476142a59de495362bb1b71cb8a0f970b9
EngineerSahil <77582744+EngineerSahil@users.noreply.github.com>
2022-08-26 12:13:17 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// clock hand variable
+
+let hourhand = document.getElementById("hourhand");
+let minhand = document.getElementById("minhand")
+let sechand = document.getElementById("sechand");
+
+// loop
+setInterval(() => {
+    // time variable
+    let time = new Date();
+    let hour = time.getHours();
+    let min = time.getMinutes()
+    let sec = time.getSeconds();
+
+    // maths
+
+    let hr = hour*30 + min/2;
+    let mr = min*6;
+    let sr = sec*6;
+
+    // hand rotation
+
+    hourhand.style.transform = `rotate(${hr}deg)`
+    minhand.style.transform = `rotate(${mr}deg)`
+    sechand.style.transform = `rotate(${sr}deg)`
+
+}, 1000);
